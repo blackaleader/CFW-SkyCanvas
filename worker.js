786fc2d0aa51ab216328fe079cbda9346c48d462
Arman Leader <128156131+blackaleader@users.noreply.github.com>
2024-06-07 23:56:26 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
+addEventListener('scheduled', event => {
+  event.waitUntil(handleScheduled(event));
 });
 
-async function handleRequest(request) {
+async function handleScheduled(event) {
   // Discord webhook URL (store this in environment variables for security)
-  const webhookUrl = '';
+  const webhookUrl = 'https://discord.com/api/webhooks/1243227062135951474/IwGtozbba6-mAt1cZ_sxXgT2fwjtr-J_3HCwS50q7PPgohpiSxTquifOhH7Q5HTs63X9';
 
   try {
     // Fetch a random quote
@@ -20,7 +20,6 @@ async function handleRequest(request) {
     const weatherResponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6944,37.2761,36.2981,52.5244,43.7001,49.2497&longitude=51.4215,49.5886,59.6057,13.4105,-79.4163,-123.1193&current=temperature_2m,is_day&timezone=auto');
     const weatherData = await weatherResponse.json();
 
-
     const timeResponse = await fetch('https://api.keybit.ir/time');
     const timeData = await timeResponse.json();
 
@@ -28,8 +27,6 @@ async function handleRequest(request) {
     const jalaliDate = timeData.date.full.official.iso.en;
     const gregorianDate = timeData.date.other.gregorian.iso.en;
     const ghamariDate = timeData.date.other.ghamari.iso.en;
-    // const dayEvent = timeData.date.day.events.holy ? timeData.date.day.events.holy.text : "No event for today";
-    // console.log(timeData.date.day.events)
     let dayEvent = "No event for today";
 
     if (timeData.date.day.events.local) {
@@ -40,9 +37,6 @@ async function handleRequest(request) {
       dayEvent = `Holy Event: ${timeData.date.day.events.holy.text}`;
     }
 
-
-
-
     // Embedded message payload
     const payload = {
       embeds: [
@@ -51,32 +45,32 @@ async function handleRequest(request) {
           description: `**Jalali Date:** ${jalaliDate}\n**Gregorian Date:** ${gregorianDate}\n**Ghamari Date:** ${ghamariDate}\n**Day Event:** ${dayEvent}`,
           fields: [
             {
-              "name": "⛅ Tehran ,Iran :",
+              "name": "⛅ Tehran, Iran:",
               "value": `${weatherData[0].current.temperature_2m}°C, ${weatherData[0].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
             {
-              "name": "⛅ Rasht ,Iran :",
+              "name": "⛅ Rasht, Iran:",
               "value": `${weatherData[1].current.temperature_2m}°C, ${weatherData[1].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
             {
-              "name": "⛅  Mashhad ,Iran :",
+              "name": "⛅ Mashhad, Iran:",
               "value": `${weatherData[2].current.temperature_2m}°C, ${weatherData[2].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
             {
-              "name": "⛅Berlin ,Germany :",
+              "name": "⛅ Berlin, Germany:",
               "value": `${weatherData[3].current.temperature_2m}°C, ${weatherData[3].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
             {
-              "name": "⛅Toronto ,Canada :",
+              "name": "⛅ Toronto, Canada:",
               "value": `${weatherData[4].current.temperature_2m}°C, ${weatherData[4].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
             {
-              "name": "⛅Vancouver ,Canada :",
+              "name": "⛅ Vancouver, Canada:",
               "value": `${weatherData[5].current.temperature_2m}°C, ${weatherData[5].current.is_day ? 'Day' : 'Night'}`,
               "inline": true
             },
